d9302032e5c27ef883d42a6d9a6c45cc2e4bf0a5
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2024-02-21 08:23:02 -0300

    add destructured and rest parameters

---
+++ modules/02_function_types/04_destructured_and_rest_parameters.ts
@@ -0,0 +1,27 @@
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#rest-parameters-and-arguments
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#parameter-destructuring
+
+
+// Do your normal destructuring just like JavaScript, when you want to type something in a destructuring providing a default value will you a type already, so if you have default values you don't need to provide a type. But if you have something that is not a default value you need to provide a type. It can't be inline, it has to be a type for the whole object.
+
+type Options = {
+  debugMode?: boolean;
+  identLevel?: number;
+}
+
+function printNameAndAge(name: string, { debugMode = false, identLevel }: Options = {}) {
+  console.log(name, debugMode, identLevel);
+}
+
+printNameAndAge('asas')
+
+
+// Anytime you use the rest operator you need to make sure to use the array syntax, it'll always return an array
+function sum(...nums: number[]) {
+  return
+}
+
+sum(1, 2, 3)
+
+
+export default {}
