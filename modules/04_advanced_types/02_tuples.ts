b1cca865db6cbe6385d655546d4b2123fced64e3
João Vitor <joaovitorfelixcontato@gmail.com>
2024-02-27 09:24:04 -0300

    add tuples

---
+++ modules/04_advanced_types/02_tuples.ts
@@ -0,0 +1,14 @@
+// https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+
+// A tuple type is another sort of Array type that knows exactly how many elements it contains, and exactly which types it contains at specific positions.
+
+const person = {
+  name: 'John',
+  age: 30,
+}
+
+type Tuple = [string, boolean]
+const a: Tuple = ["as", true]
+
+
+export default {}
