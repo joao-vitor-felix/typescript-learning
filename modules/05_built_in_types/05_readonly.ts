830d510096e2573c97d2a9461674e6f03f486176
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-06 09:52:44 -0300

    add readonly

---
+++ modules/05_built_in_types/05_readonly.ts
@@ -0,0 +1,12 @@
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
+
+// Readonly turns all properties into readonly, meaning you can't reassign them. It does the same as as const, but for object types.
+
+type Todo = {
+  title: string
+  completed: boolean
+}
+
+type FinalTodod = Readonly<Todo>
+
+export {}
