87e9bfa00e529f32bfa17520753a6e4703f1d510
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-06 09:52:38 -0300

    add record

---
+++ modules/05_built_in_types/04_record.ts
@@ -0,0 +1,16 @@
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type
+
+// Constructs an object type whose property keys are Keys and whose property values are Type
+
+type Person = {
+  name: string
+  age: number
+}
+
+// type PeopleGroupedByName = {
+//   [index: string]: Person[]
+// }
+
+type PeopleGroupedByName = Record<Person["name"], Person[]> // same as above
+
+export {}
