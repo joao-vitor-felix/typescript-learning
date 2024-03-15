ad7372e94fa7e74ffabd71c1196f786be46e5081
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-14 21:38:58 -0300

    update typing variable as functions code and comments

--- modules/02_function_types/05_typing_variables_as_functions.ts
+++ modules/02_function_types/05_typing_variables_as_functions.ts
@@ -1,16 +1,16 @@
 // https://www.typescriptlang.org/docs/handbook/2/functions.html#function-type-expressions
 
-// The simplest way to describe a function is with a function type expression. These types are syntactically similar to arrow functions
+// When you're defining a variable as a function type, you do it with the arrow function syntax.
 
 function sumWithCallback(a: number, b: number, cb: (sum: number) => void) {
   cb(a+b)
 }
 
-// The only difference when you're defining the return type in a function like this, you do it with type annotations, but when you're defining something as a function, as a type, you do it with the arrow function syntax.
+// When you're defining something as a function type, you do it with the arrow function syntax.
+type PrintNameFunc = (name: string) => number
 
-type PrintNameFunc = (name: string) => void
-
-function f(name: string) {
+// When you're defining the return type in a function, you do it with type annotations
+function f(name: string): number {
   return 2
 }
 
