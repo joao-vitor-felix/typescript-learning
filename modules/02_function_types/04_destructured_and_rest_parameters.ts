698faf7dfb31402f24df0f8e6ea69f6be61efbca
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-14 21:22:12 -0300

    update destructure and rest parameters code and comments

--- modules/02_function_types/04_destructured_and_rest_parameters.ts
+++ modules/02_function_types/04_destructured_and_rest_parameters.ts
@@ -1,20 +1,22 @@
 // https://www.typescriptlang.org/docs/handbook/2/functions.html#rest-parameters-and-arguments
 // https://www.typescriptlang.org/docs/handbook/2/functions.html#parameter-destructuring
 
-// When you want to type something in a destructuring, providing a default value will give you a type already, so if you have default values you don't need to provide a type. 
-// But if you have something that is not a default value, you need to provide a type. It can't be inline, it has to be a type for the whole object.
+// When you want to type something in a destructuring, providing a default value will give you a type already, so if you have default value you don't need to provide a type.
+function printNameAndAge(name: string, { debugMode = false } = {}) {
+  console.log(name, debugMode);
+}
 
 type Options = {
   debugMode?: boolean;
   identLevel?: number;
 }
 
-function printNameAndAge(name: string, { debugMode = false, identLevel }: Options = {}) {
+// But if you have something that is not a default value, you need to provide a type. It can't be inline, it has to be a type for the whole object.
+function printNameAndAge2(name: string, { debugMode = false, identLevel }: Options = {}) {
   console.log(name, debugMode, identLevel);
 }
 
-printNameAndAge('asas')
-
+printNameAndAge2('asas')
 
 // Anytime you use the rest operator you need to make sure to use the array syntax, it'll always return an array
 function sum(...nums: number[]) {
@@ -22,6 +24,5 @@ function sum(...nums: number[]) {
 }
 
 sum(1, 2, 3)
-
 
 export default {}
