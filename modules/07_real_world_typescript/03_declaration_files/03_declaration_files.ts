b6992b0174f8c215d50ebacb7a7893f165fc6423
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-27 09:53:35 -0300

    add declaration files

---
+++ modules/07_real_world_typescript/03_declaration_files/03_declaration_files.ts
@@ -0,0 +1,14 @@
+// https://www.typescriptlang.org/docs/handbook/namespaces.html
+// https://www.typescriptlang.org/docs/handbook/namespaces-and-modules.html
+
+// Every d.ts file is a declaration file, it's generally used to add types to libraries as well to built-in types.
+// All d.ts file is automatically inferred and imported everywhere that you use your code.
+// If you want to add types to a library that do not have types you need to add a d.ts file, it doesn't matter how you call it, but generally you'll name the same as the library name.
+// The name of the module must match the name of the library you want to add types to.
+// Sometimes you might want to overwrite the global values that are available for you. When you overwrite globals you need to make sure you export something in your file, you don't need to do that when declaring a module.
+
+import { times } from "lodash";
+
+times(4);
+
+console.superLog();
