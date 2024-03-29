33f362fbbd2d4147f0cec4c009502e6ae349e979
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-29 16:07:55 -0300

    add useRef

---
+++ modules/08_typing_react/04_useRef.tsx
@@ -0,0 +1,17 @@
+// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/hooks#useref
+
+import { useRef } from "react";
+
+function App() {
+  // 1. DOM element ref: To access a DOM element: provide only the element type as argument, and use null as initial value. In this case, the returned reference will have a read-only .current that is managed by React. TypeScript expects you to give this ref to an element's ref prop
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // 2. Mutable value ref: To have a mutable value: provide the type you want, and make sure the initial value fully belongs to that type
+  const value = useRef(0)
+  const num = useRef<number>()
+
+  value.current = 5
+  num.current = 5
+
+  return <input ref={inputRef}></input>
+}
