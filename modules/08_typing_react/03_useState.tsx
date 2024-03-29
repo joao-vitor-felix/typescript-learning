666153f4f07f6689fd9703c268662fab186ff133
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-29 12:50:49 -0300

    add useState

---
+++ modules/08_typing_react/03_useState.tsx
@@ -0,0 +1,19 @@
+// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/hooks#usestate
+
+import { useState } from "react";
+
+type User = {
+  name: string;
+  age: number;
+};
+
+
+function App() {
+  // You can type as a generic to tell the type of the state or you can use the type inference by your default value
+  const [user, setUser] = useState<User | null>(null);
+  const [state, setState] = useState(false);
+  const [text, setText] = useState<string>('');
+  const [number, setNumber] = useState<number[]>([]);
+
+  return <></>
+}
